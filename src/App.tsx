9ea9718dda18f24c2f4ec7060596c288d2ddd258
lcsfort <lcsfort@pop-os.localdomain>
2023-05-02 21:54:32 -0100

    feat: Create the Home page with the input and display component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">Bin2Dec</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { bin2dec, isValidBinaryNumber } from "../utils/bin2dec";
+import OldTv from "../components/OldTv";
+import "./styles.css";
+
+function Home() {
+  const [value, setValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  return (
+    <div className="home">
+      <h1>Bin2Dec</h1>
+      <h3>Simple binary to decimal number converter</h3>
+      <OldTv>
+        <h1 className="text-view">{isValid ? bin2dec(value) : 0}</h1>
+      </OldTv>
+      <div className="input-box">
+        <div className="input-container">
+          <input
+            autoFocus
+            type="text"
+            onChange={(event) => {
+              const binNumb = event.target.value;
+
+              const isValidBin = isValidBinaryNumber(binNumb);
+
+              setIsValid(isValidBin);
+              setValue(binNumb);
+            }}
+          />
+        </div>
+        {value.trim() !== "" && !isValid && (
+          <label>Please type a valid binary number. E.g. 11111001100</label>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
