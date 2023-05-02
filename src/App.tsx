c0a48b693160e5d8679a6221480c48496d444c0d
lcsfort <lcsfort@pop-os.localdomain>
2023-05-02 21:12:08 -0100

    style: Update the layout and change the backround color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      Bin2Dec
-    </div>
-  );
+  return <div className="App">Bin2Dec</div>;
 }
 
 export default App;
